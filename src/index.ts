d54b9daf55c47e657cb8aa93fcfe43014ca3f1fb
LauraKirby <laurakirby26@gmail.com>
2023-05-13 20:36:19 -0700

    add HTTP middleware framework - Koa

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { AppDataSource } from "./data-source";
 import { User } from "./entity/user";
+const Koa = require("koa");
+const app = new Koa();
 
 AppDataSource.initialize()
   .then(async () => {
@@ -14,6 +16,32 @@ AppDataSource.initialize()
     console.log("Loading users from the database...");
     const users = await AppDataSource.manager.find(User);
     console.log("Loaded users: ", users);
+
+    // setup server
+    console.log("setting up server");
+    app.use(async (ctx: any, next: any) => {
+      await next();
+      const rt = ctx.response.get("X-Response-Time");
+      console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+    });
+
+    // an example of how to add middleware the request/response cycle
+    // x-response-time
+    app.use(async (ctx: any, next: any) => {
+      const start = Date.now();
+      await next();
+      const ms = Date.now() - start;
+      ctx.set("X-Response-Time", `${ms}ms`);
+    });
+
+    // if you wanted to add custom logging for any type of error
+    // you would add that here.
+    app.on("error", (err: any) => {
+      console.log("server error", err);
+    });
+
+    console.log("starting server");
+    app.listen(3000);
   })
   .catch((error) => {
     console.log(error);
